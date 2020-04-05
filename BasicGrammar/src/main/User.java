9b2da05a64b465c94a105ef6985e35e465358361
Sunjae <nalala_sj@naver.com>
2020-04-05 21:12:51 +0900

    create project

---
+++ BasicGrammar/src/main/User.java
@@ -0,0 +1,15 @@
+package main;
+
+public class User {
+	private int a;
+	private int b;
+	
+	public User(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int add() {
+		return a+b;
+	}
+}
